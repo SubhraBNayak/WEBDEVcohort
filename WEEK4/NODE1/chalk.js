610e34454d2a8f2d89d693299fc1a58fa5f53c94
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2025-06-23 23:50:56 +0530

    worked with some internal/external libraries in node.js...Understood what package.json and package-lock.json does

---
+++ WEEK4/NODE1/chalk.js
@@ -0,0 +1,7 @@
+/*
+    EXTERNAL DEPENDENCIES (NEEDS TO BE INSTALLED)
+*/
+const chalk = require('chalk');
+
+console.log(chalk.red('This is red text'));
+console.log(chalk.green.bold('This is bold green text'));
