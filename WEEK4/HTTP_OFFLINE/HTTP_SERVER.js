10b90ccba549600e3faa2f10f7ff20c3ce4a4620
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2025-06-24 23:59:46 +0530

    wrote an http server

---
+++ WEEK4/HTTP_OFFLINE/HTTP_SERVER.js
@@ -0,0 +1,24 @@
+/*
+    ###################### WE WILL CREATE OUR OWN HTTP SERVER #######################
+    That will be able to get, post, put and delete data from the server database and return data to the browser
+ */
+
+const express = require('express');
+
+function calculatesum(n){
+    let ans = 0;
+    for (let index = 0; index < n; index++) {
+        ans = ans + index
+    }
+    return ans;
+}
+
+const app = express();
+
+app.get("/", function(req, res){
+    const n = req.query.n
+    const ans = calculatesum(n)
+    res.send(ans.toString())
+})
+
+app.listen(3000)
