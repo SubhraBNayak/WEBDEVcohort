1afe48e1811c914a069ac6d3189caf22f46df895
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2025-06-25 23:59:38 +0530

    commiting a server

---
+++ WEEK4/HTTP_OFFLINE/DOCTOR_SERVER.js
@@ -0,0 +1,20 @@
+//THE SERVER IS LIKE A HOSPITAL WHERE PATIENT COMES TO CHECK ON KIDNEYS
+const express = require('express')
+
+const app = express()
+
+const users = [{
+    name : 'john',
+    kidney:[
+        {
+            healthy : false
+        }
+    ]
+}]
+
+app.get("/", function(req, res)){
+    const johnkidneys = users[0].kidney;
+    console.log(johnkidneys)
+}
+
+app.listen(3000)
