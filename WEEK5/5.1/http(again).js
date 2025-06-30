6dd291d1d8a4db7256f2394e64e83635025296c5
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2025-06-30 22:58:15 +0530

    introduced a middleware that checks if the entered query is integer or not?

--- WEEK5/5.1/http(again).js
+++ WEEK5/5.1/http(again).js
@@ -1,18 +1,30 @@
 /*
-    ######################### WRITING AN HTTP SERVER WITH FOUR GET ENDPOINTS #########################
+    ######################### WRITING AN HTTP SERVER WITH FOUR GET ENDPOINTS FOR A CALCULATOR #########################
     This HTTP server consists of four end-points (all GET).
     These four endpoints represent four features.
     Feature 1 : add
     Feature 2 : subtract
     Feature 3 : multiply
     Feature 4 : divide
+    Feature 5 : check if inputted query is an integer or not (use middlewares)
+                so every route-handler will have to go through a middleware 
+                known as checkNum
 */
 
 const express = require("express")
 const app = express()
 
+function checkNum(req, res, next){
+    p = parseFloat(req.query.p)
+    q = parseFloat(req.query.q)
+    if (isNaN(p) && isNaN(q)) {
+        res.status(400).send("Entered query is not a number")
+    }else{
+        next()
+    }
+}
 
-app.get("/sum", function(req, res){
+app.get("/sum", checkNum , function(req, res){
     p = parseFloat(req.query.p)
     q = parseFloat(req.query.q)
 
@@ -23,7 +35,7 @@ app.get("/sum", function(req, res){
     })
 })
 
-app.get("/subtract", function(req, res){
+app.get("/subtract", checkNum, function(req, res){
     p = parseFloat(req.query.p)
     q = parseFloat(req.query.q)
 
@@ -33,7 +45,7 @@ app.get("/subtract", function(req, res){
     })
 })
 
-app.get("/multiply", function(req, res){
+app.get("/multiply",checkNum, function(req, res){
     p = parseFloat(req.query.p)
     q = parseFloat(req.query.q)
 
@@ -43,7 +55,7 @@ app.get("/multiply", function(req, res){
     })
 })
 
-app.get("/division", function(req, res){
+app.get("/division",checkNum, function(req, res){
     p = parseFloat(req.query.p)
     q = parseFloat(req.query.q)
 
