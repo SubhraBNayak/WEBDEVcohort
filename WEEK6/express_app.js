220d711b0247087611e146125a61e6cc923944aa
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2025-07-10 23:40:50 +0530

    feat : wrote a function (generateRandomToken) that generates random token that is assigned to the user for upcoming requests

--- WEEK6/express_app.js
+++ WEEK6/express_app.js
@@ -2,12 +2,26 @@
     ###################### CREATING AN EXPRESS APPLICATION ######################
     -> ADD express as a dependency
     -> create two post route, one for sign up other one for signing in
-    -> use express.json as a middleware to parse the post   request of the body
+    -> use express.json as a middleware to parse the post request of the body
     -> create an in memory variable called users to where you store the username, password and a random token 
+    -> create a function called generateRandomToken, that generated random tokens
 */
 
 const express = require("express")
 const app = express()
+
+function generateRandomToken(){
+    const options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+                    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                    'A', 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I' , 'J' , 'K' , 'L' , 'M' , 
+                    'N' , 'O' , 'P' , 'Q' , 'R' , 'S' , 'T' , 'U' , 'V' , 'W' , 'X' , 'Y' , 'Z' , 
+                    '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9']
+    let token = ""
+    for (let index = 0; index < options.length; index++){
+        token += options[Math.floor(Math.random()*options.length)]
+    }
+    return token
+}
 
 app.use(express.json())//parses the incoming requests
 
@@ -28,7 +42,8 @@ app.post("/signup", (req,res) => {
 })
 
 app.post("/signin", (req, res) => {
-
+    generateRandomToken();
 })
 
 app.listen(3000)
+
