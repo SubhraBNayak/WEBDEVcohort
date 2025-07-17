377fb942725c91ca02271cb5dfab589eadf913e9
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2025-07-17 22:05:40 +0530

    webpage : login_frontend.html and login_css.css created a fronted for the login page of protask
    webpage : TODO_APP html css and js files to make a todo list frontend
    
    server : backend_server.js => is the backend server that provides signup/signin endpoints, stores the user data in "users" array. it also authorizes a user during sign-in, returns a jwt token upon signin

---
+++ WEEK6/WEEK6_ASSIGNMENT/backend_server.js
@@ -0,0 +1,103 @@
+/*
+    ####################### SERVER OF THE LOGIN PAGE #######################
+    1. feat => signup, pushing the user details into a users array
+            => introduce a middleware that checks if someone exists with the same username
+            two different users can't have the same username. (error 409 conflict)
+            => introduce a middleware that should check if a user with same username, password exists.
+            if so, thorw an error, "user already exists! try signing in".
+*/
+const express = require("express")
+const app = express()
+
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = "nayaksoftwarepvtltd"
+
+const cors = require("cors")
+
+let users = []
+
+function authorize_user(req, res, next){
+    const username = req.body.username
+    const password = req.body.password
+
+    for (let index = 0; index < users.length; index++) {
+        if (username === users[index].username && password === users[index].password) {
+            req.username = username
+            return next()
+        }
+    }
+    return res.status(401).send({
+        message: "User authentication failed! Check the username/password again"
+    })
+}
+
+function check_username(req, res, next){
+    const username = req.body.username
+
+    for (let index = 0; index < users.length; index++) {
+        if (username === users[index].username) {
+            return res.status(409).send({
+                message : "username already exists! try different username"
+            })
+        }
+    }
+    next()
+}
+
+function check_user(req, res, next){
+    const username = req.body.username
+    const password = req.body.password
+    
+    for (let index = 0; index < users.length; index++) {
+        if (username === users[index].username && password === users[index].password) {
+            return res.status(409).send({
+                message: "user already exists! try signing in"
+            })
+        }
+    }
+    next()
+}
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
+
+app.get("/", function(req, res){
+    console.log("backend is running!")
+})
+
+app.post("/signup", check_user, check_username, function(req, res){
+    const username = req.body.username
+    const password = req.body.password
+    
+    users.push({
+        username : username,
+        password : password
+    })
+
+    res.status(200).send({
+        message : "sign-up successful"
+    })
+})
+
+app.post("/signin", authorize_user, function(req, res){
+    const username = req.username
+    const token = jwt.sign({username}, JWT_SECRET)
+
+    res.status(200).json({
+        token : token
+    })
+})
+
+app.get("/auth", function (req, res){
+    const token = req.headers.token
+    const username = jwt.verify(token, JWT_SECRET)
+
+    if (username) {
+        res.send(200)
+    }else{
+        res.send(401)
+    }
+})
+
+app.listen(3000)
---
+++ WEEK6/WEEK6_ASSIGNMENT/public/TODO_APP_SCRIPT.js
@@ -0,0 +1,93 @@
+//we'll use this 'todos' array to store the states under 'title'
+let todos = [];
+
+/*
+    addTodo function is going to add a new state ('title') into the todos array
+    then it calls the render function to display the changes made in the browser
+*/
+function addTodo(){
+    const value = document.querySelector("input").value;
+    if(value){
+        todos.push({
+            title:document.querySelector("input").value
+        })
+        render ();
+    }
+    else{
+        alert("Blank input! Task can't be added!");
+    }
+}
+
+/*
+    function createComponent creates a basic layout that is supposed to be rendered after the render () function is called
+*/
+function createComponent(){
+    let newEl = document.createElement("div");
+    let newSpan = document.createElement("span");
+    let deleteBtn = document.createElement("button");
+    let editBtn = document.createElement("button");
+    newEl.appendChild(newSpan);
+    newEl.appendChild(deleteBtn);
+    newEl.appendChild(editBtn);
+    return newEl;
+}
+
+/*
+    render function uses todos array as the ultimate source of truth
+    it fetches the states from todos and updates the components created for each state
+    then displays it in the web
+ */
+function render(){
+    document.getElementById("todo-container").innerHTML = '';
+    let newEl;
+    for (let index = 0; index < todos.length; index++) {
+        newEl = createComponent()
+        newEl.id = `todo-${index}`;
+        newEl.className = `divEl`
+        newEl.querySelector("span").innerText = (index+1) + '.' + todos[index].title;
+        newEl.querySelector("span").className = `spanEl`
+        newEl.querySelectorAll("button")[0].innerText = "Delete";
+        newEl.querySelectorAll("button")[0].onclick = () => deleteTask(index);
+        newEl.querySelectorAll("button")[0].className = `deleteEl`
+        newEl.querySelectorAll("button")[1].innerText = "Edit";
+        newEl.querySelectorAll("button")[1].id = `edit-${index}`;
+        newEl.querySelectorAll("button")[1].className = `editEl`;
+        newEl.querySelectorAll("button")[1].onclick = () => editTask(index);
+        document.getElementById("todo-container").appendChild(newEl);
+    }
+}
+
+/*
+    deleteTask function only updates the todos array, only deletes the task that was clicked by the user to be deleted
+    render () function is called again inorder to render the changes
+*/
+function deleteTask(index){
+    todos.splice(index,1);
+    render (); //now when render is called the entire list needs to rendered again
+}
+
+function editTask(index){
+    let prev_value = todos[index].title
+    let prev_span = document.getElementById(`todo-${index}`).querySelector("span");
+    prev_span.parentNode.removeChild(prev_span);
+    let new_input = document.createElement("input");
+    new_input.className = `input2`
+    new_input.value = prev_value;
+    document.getElementById(`todo-${index}`).appendChild(new_input);
+    new_input.id = `input-${index}`
+    document.getElementById(`edit-${index}`).innerText = "Save";
+    document.getElementById(`edit-${index}`).classList.add("saveMode");
+    document.getElementById(`edit-${index}`).onclick = () => updateTask(index);
+}
+
+function updateTask(index){
+    const value = document.getElementById(`input-${index}`).value;
+    if(value){
+        todos[index].title = value;
+        render ();
+        document.getElementById(`edit-${index}`).classList.remove("saveMode");
+    }
+    else{
+        alert("Blank input! Task can't be added!")
+    }
+}
