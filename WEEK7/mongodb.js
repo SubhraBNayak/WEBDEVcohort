e5024c8e15d8308ee72421d62e3c9b04e0e4dc29
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2025-07-28 23:33:04 +0530

    database: define schemas and create Mongoose models
    - Created UserModel with fields: email, password, name
    - Created TodoModel with fields: userId, title, done
    - mongodb.js now contains schema definitions and acts as the data model layer of the backend

--- WEEK7/mongodb.js
+++ WEEK7/mongodb.js
@@ -1,21 +1,27 @@
+/*
+    ####################### THIS IS A DATA MODEL LAYER #######################
+    -> this contains the definition of the schemas
+    -> this defines the structures of the data in database
+*/
+
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const ObjectId = mongoose.ObjectId
 
 const User = new Schema({
-    email : {type : String , unique : false},
+    email : {type : String, unique : false},
     password : String,
-    name : String
+    name : String    
 })
 
 const Todo = new Schema({
+    userId : ObjectId,
     title : String,
-    done : Boolean,
-    userId : ObjectId
+    done : {type : Boolean, default : false}
 })
 
-const UserModel = mongoose.model('users', User)
-const TodoModel = mongoose.model('todos', Todo)
+const UserModel = mongoose.model("users", User)
+const TodoModel = mongoose.model("todos", Todo)
 
 module.exports = {
     UserModel,
