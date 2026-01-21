2b991e2af9c3cbf701de4ff44c77228307fc9ea7
Subhra Bikiran Nayak <subhrabikiran@gmail.com>
2026-01-21 19:06:08 +0530

    a very basic react application

--- week10-react2/src/App.jsx
+++ week10-react2/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Hello World</h1>
+      <Counter></Counter>
+    </div>
+  )
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  
+  // setInterval(() => {
+  //   setCount(count + 1);
+  // }, 1000);
+
+  function IncreaseCount(){
+    setCount(count + 1);
+  }
+
+  function DecreaseCount(){
+    setCount(count - 1);
+  }
+
+  function ResetCount(){
+    setCount(0);
+  }
+
+  return(
+    <div>
+      <h1 id="text">{count}</h1>
+      <button onClick={IncreaseCount}>Increase count</button>
+      <button onClick={DecreaseCount}>Decrease Count</button>
+      <button onClick={ResetCount}>Reset Count</button>
+    </div>
   )
 }
 
